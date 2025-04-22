d5473075835f13074556a4497eb6ad202e1743ba
dfinke <finked@hotmail.com>
2025-04-22 17:42:55 -0400

    Remove Write-Log function and ensure logging is handled correctly in server.template.ps1

--- template/server.template.ps1
+++ template/server.template.ps1
@@ -1,3 +1,5 @@
+Set-LogFile "$PSScriptRoot\mcp_server.log"
+
 <#
 .SYNOPSIS
     Adds two numbers together.
@@ -30,17 +32,6 @@ $toolsListJson = Register-MCPTool Invoke-Addition
 
 
 
-function Write-Log {
-    param(
-        [Parameter(Mandatory = $true)]        
-        [object]$LogEntry # Changed parameter to accept an object (e.g., Hashtable or PSObject)
-    )
-    $logFile = "$PSScriptRoot\mcp_server.log"
-    # Add a timestamp to the log entry
-    $logObject = $LogEntry | Select-Object *, @{Name = 'Timestamp'; Expression = { (Get-Date -Format 'o') } }
-    # Convert the object to a JSON string and append to the log file
-    $logObject | ConvertTo-Json -Depth 10 -Compress | Add-Content -Path $logFile
-}
 
 function Invoke-HandleRequest {
     param([object]$request)
