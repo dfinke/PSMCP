b63f240862b5b951ccff60916614ef8232874d8d
dfinke <finked@hotmail.com>
2025-04-28 16:11:01 -0400

    Add MCPServer.ps1 script with Invoke-Addition function

---
+++ __tests__/MCPServer.ps1
@@ -0,0 +1,29 @@
+#Requires -Module PSMCP
+
+Set-LogFile "$PSScriptRoot\mcp_server.log"
+
+<#
+.SYNOPSIS
+    Adds two numbers together.
+
+.DESCRIPTION
+    The Invoke-Addition function takes two numeric parameters and returns their sum.
+
+.PARAMETER a
+    The first number to add.
+
+.PARAMETER b
+    The second number to add.
+#>
+function Global:Invoke-Addition {
+    param(
+        [Parameter(Mandatory)]
+        [double]$a, 
+        [Parameter(Mandatory)]
+        [double]$b
+    ) 
+
+    $a + $b
+}
+
+Start-McpServer Invoke-Addition
